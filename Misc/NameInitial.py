6ce0a9aef8add67b6fcf7fe68e5f2385b977b242
Ninh Nguyen <95522691+ninhnguyen01@users.noreply.github.com>
2024-08-23 21:32:03 -0700

    Add files via upload

---
+++ Misc/NameInitial.py
@@ -0,0 +1,26 @@
+print("N       N" + " " + " N       N") 
+print("N N     N" + " " + " N N     N")
+print("N   N   N" + " " + " N   N   N")
+print("N     N N" + " " + " N     N N")
+print("N       N" + " " + " N       N")
+print("N       N" + " " + " N       N")
+print("N       N" + " " + " N       N")
+
+# Ninh Nguyen
+# I am a graduate student
+
+print("N       N") 
+print("N N     N")
+print("N   N   N")
+print("N     N N")
+print("N       N")
+print("N       N")
+print("N       N")
+
+print("N       N" + " " + " N       N") 
+print("N N     N" + " " + " N N     N")
+print("N   N   N" + " " + " N   N   N")
+print("N     N N" + " " + " N     N N")
+print("N       N" + " " + " N       N")
+print("N       N" + " " + " N       N")
+print("N       N" + " " + " N       N")
