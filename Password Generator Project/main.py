c181b04abca2244f1c84a91f3e5b632593aaabfc
Ninh Nguyen <95522691+ninhnguyen01@users.noreply.github.com>
2024-09-16 15:15:04 -0700

    Add files via upload

---
+++ Password Generator Project/main.py
@@ -0,0 +1,33 @@
+import re
+import string
+import secrets
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+
+        constraints = [(nums, r'\d'),
+                       (lowercase, r'[a-z]'),
+                       (uppercase, r'[A-Z]'),
+                       (special_chars, fr'[{symbols}]')]
+        
+        # Check constraints
+        if all(constraint <= len(re.findall(pattern, password)) for constraint, pattern in constraints):
+            break
+
+    return password
+
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
